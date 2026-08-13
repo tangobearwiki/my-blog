7493ff97fc61a9569b9b5819108a0a3336cc11b6
Tangobear <tangobearwiki@gmail.com>
2026-08-14 00:38:38 +0800

    🎉 Railway 博客 - 完整后端 + 前端 + 后台管理

--- functions/hello.js
+++
@@ -1,5 +0,0 @@
-export async function onRequest(context) {
-  return new Response(JSON.stringify({ status: 'ok', message: 'Functions is working!' }), {
-    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
-  });
-}
